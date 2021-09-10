9a8e4757c9cc16d3d869101752fcb57430c80f68
HyunJung <HyunJungC.Dev@gmail.com>
2021-09-10 19:02:56 +0900

    2021-09-19: 10분
    - 비트 연산자로 바꾸기

--- Programmers/KAKAOBLINDRECRUITMENT/2018/1_비밀지도.js
+++ Programmers/KAKAOBLINDRECRUITMENT/2018/1_비밀지도.js
@@ -1,26 +1,13 @@
 function solution(n, arr1, arr2) {
-  const map1 = arr1.map(rowNum => {
-    const binary = rowNum.toString(2);
-    return binary.length < n
-      ? Array.from({ length: n - binary.length }, () => '0').join('') + binary
-      : binary;
-  });
-
-  const map2 = arr2.map(rowNum => {
-    const binary = rowNum.toString(2);
-    return binary.length < n
-      ? Array.from({ length: n - binary.length }, () => '0').join('') + binary
-      : binary;
-  });
-
-  return map1.map((map1Row, idx) => {
-    const map1RowSplit = map1Row.split('');
-    const map2RowSplit = map2[idx].split('');
-    return map1RowSplit
-      .map((map1Bit, idx) =>
-        map1Bit === '1' || map2RowSplit[idx] === '1' ? '#' : ' '
-      )
-      .join('');
+  return arr1.map((map1Row, idx) => {
+    const binary = (map1Row | arr2[idx]).toString(2);
+    return (
+      binary.length < n
+        ? Array.from({ length: n - binary.length }, () => '0').join('') + binary
+        : binary
+    )
+      .replace(/1/g, '#')
+      .replace(/0/g, ' ');
   });
 }
 
